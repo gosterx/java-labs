f5f2446ee140352cd8715a3185b25e647fdd7370
Egor <egor.katlinskii@yandex.by>
2020-10-31 16:06:15 +0300

    Rename IControl,PControl

---
+++ Lab4/src/main/java/Task2/IControl.java
@@ -0,0 +1,17 @@
+package Task2;
+
+import java.util.Scanner;
+
+public class IControl {
+    static Scanner  s = new Scanner(System.in);
+    public static void fillingTheArray(double[] array){
+        for(int i=0;i<array.length;i++){
+            array[i]= s.nextDouble();
+        }
+    }
+
+    public static int sizeNumberInput(){
+        return s.nextInt();
+    }
+
+}
---
+++ Lab4/src/main/java/Task2/PControl.java
@@ -0,0 +1,7 @@
+package Task2;
+
+public class PControl {
+    public static void printMsg(String msg){
+        System.out.println(msg);
+    }
+}
