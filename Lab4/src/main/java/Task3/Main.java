a7bd3f4a1d857372c47b7166c326f141e598b16d
Egor <egor.katlinskii@yandex.by>
2020-10-31 18:04:59 +0300

    app Task3

---
+++ Lab4/src/main/java/Task3/Main.java
@@ -0,0 +1,17 @@
+package Task3;
+
+import genericClasses.IControl;
+import genericClasses.PControl;
+
+public class Main {
+    public static void main(String[] args) {
+        PControl.printMsg("Enter 2 numbers: ");
+        if(WorkWithNumber.ParityStudy(IControl.NumberInput(),IControl.NumberInput()))
+        {
+            PControl.printMsg("The number is a multiple of the sequence!");
+        }
+        else{
+            PControl.printMsg("The number is not a multiple!");
+        }
+    }
+}
---
+++ Lab4/src/main/java/Task3/WorkWithNumber.java
@@ -0,0 +1,8 @@
+package Task3;
+
+class WorkWithNumber {
+
+    public static boolean ParityStudy(int numberOne,int numberTwo){
+        return numberOne%numberTwo==0;
+    }
+}
